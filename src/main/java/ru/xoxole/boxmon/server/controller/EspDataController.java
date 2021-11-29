d9dc2c5fa55931d59ba0ea0bdc922b5dea4bccaa
kashnikovnn <nkashnikov@vtb.ru>
2021-11-29 20:01:43 +0300

    fix cors

--- src/main/java/ru/xoxole/boxmon/server/controller/EspDataController.java
+++ src/main/java/ru/xoxole/boxmon/server/controller/EspDataController.java
@@ -7,18 +7,20 @@ import org.springframework.web.bind.annotation.*;
 import ru.xoxole.boxmon.server.model.EspData;
 import ru.xoxole.boxmon.server.service.EspDataService;
 
+import javax.websocket.server.PathParam;
 import java.util.List;
 
 @RestController
 @RequestMapping(("/espdata"))
 @AllArgsConstructor
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class EspDataController {
 
 
     private final EspDataService espDataService;
 
-    @GetMapping("/getByLastHours")
-    public List<EspData> findEspData(Integer hours) {
+    @GetMapping("/getByLastHours/{hours}")
+    public List<EspData> findEspData(@PathVariable Integer hours) {
         return espDataService.getByLastHours(hours);
     }
 
